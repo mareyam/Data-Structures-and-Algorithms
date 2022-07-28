b790a8760df563951576ecb4059c363d8777b7a0
Maryam Naveed <63284481+mareyam@users.noreply.github.com>
2022-07-28 06:57:43 -0700

    Update deleteNode.java

--- LeetCode/LinkedList/deleteNode.java
+++ LeetCode/LinkedList/deleteNode.java
@@ -1,5 +1,7 @@
 // Online Java Compiler
 // Use this editor to write, compile and run your Java code online
+https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+
 
 class HelloWorld {
     public static void main(String[] args) {
